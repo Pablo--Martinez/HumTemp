cbcf27e0a3ef7399e2174b124522692fec412487
Pablo <pablo@C3PIO.(none)>
2013-08-23 15:31:53 -0300

    Delete Aplicacion.py and rename Aplicacion2.py to Aplicacion.py

--- Aplicacion.py
+++ Aplicacion.py
@@ -5,16 +5,113 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 import os
+import subprocess
+import sys
 
 ctr = "control"
 separador = ";"
+pines = ["4","17","18","22","23","24","25","27"]
+errores = ["Ya hay una sesion activa..","Debe terminar la sesion activa para bajar datos..",
+			"No existe sesion activa actualmente.."]
 
+def IniciarCensado(nombre,ciclo,terminal):
+		"""
+		Inicia el ciclo de sensado, los datos son almacenados cada "ciclo" minutos
+		bajo el nombre de "nombre", se asume que no esta censando actualmente
+		"""
+		db = PostgreSQL.PostgreSQL()
+		try: #Intento modificar control a la db
+			db.UpdateRegisterInTable(ctr,["id",1],["name",nombre])
+			db.UpdateRegisterInTable(ctr,["id",1],["ciclo",ciclo-1])
+			db.UpdateRegisterInTable(ctr,["id",1],["veces",0])
+			db.UpdateRegisterInTable(ctr,["id",1],["status",1])
+		except: #No existe control
+			db.CreateTable(ctr,["name VARCHAR","ciclo INTEGER","veces INTEGER","status INTEGER"])
+			db.InsertRegisterInTable(ctr,[nombre,ciclo-1,0,1])
+		db.CloseDB()
+		if (not terminal):
+			gtk.main_quit()
+			os.system(os.getcwd() + "/Aplicacion2.py" )
+				
+def TerminarCensado(terminal):
+	"""
+	Cancela el censado actual, asume que actualmente se esta censando
+	"""
+	db = PostgreSQL.PostgreSQL()
+	db.UpdateRegisterInTable(ctr,["id",1],["status",0])
+	db.CloseDB()
+	if (not terminal):
+		gtk.main_quit()
+		os.system(os.getcwd() + "/Aplicacion2.py" )
+			
+def BajarDatos(): 
+	"""
+	Baja los datos de la db del ulitmo ciclo de censado, es decir donde el 
+	nombre es el guardado el almacenado en control.
+	Asume que el ciclo de censado ya termino
+	"""
+	db = PostgreSQL.PostgreSQL()
+	row = db.SelectFromTable(ctr,["id",1])
+	rows = db.SelectFromTable("register",["name",row[0][1]])
+	if (len(rows) > 0):
+		f = open(row[0][1] + "_datos.txt","w")
+		for elem in rows:
+			line = elem[1] + separador + str(elem[2]) + separador + str(elem[3]) + separador + str(elem[4]) + separador + str(elem[5])
+			f.write(line + "\n")
+		f.close()
+	db.CloseDB()
+	
+def Estado():
+	"""
+	Retorna el estado actual del censado
+	"""
+	db = PostgreSQL.PostgreSQL()
+	row = db.SelectFromTable(ctr,["id",1])
+	if (row[0][4] == 1):
+		db.CloseDB()
+		return 1
+	else:
+		db.CloseDB()
+		return 0
+	
+def Nombre():
+	"""
+	Retorna el nombre del censado actual
+	"""
+	db = PostgreSQL.PostgreSQL()
+	row = db.SelectFromTable(ctr,["id",1])
+	db.CloseDB()
+	return row[0][1]
+
+def Ciclo():
+	"""
+	Retorna el ciclo del censado actual
+	"""
+	db = PostgreSQL.PostgreSQL()
+	row = db.SelectFromTable(ctr,["id",1])
+	db.CloseDB()
+	return row[0][2] + 1
+
+class GUI_Error():
+	def __init__(self,texto):
+		#error = gtk.MessageDialog()
+		error = gtk.MessageDialog(parent=None, flags=0, buttons=gtk.BUTTONS_OK)
+		error.set_title("BioGuard-Error!")
+		error.set_size_request(400,150)
+		error.connect("delete-event", gtk.main_quit)
+		label = gtk.Label(texto)
+		error.vbox.pack_start(label)
+		error.show_all()
+		error.run()
+		error.destroy()
+		#error.main()
+		
 class GUI_app():
 	def __init__(self):
 		#Creo la ventana
 		gui = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		gui.set_title("BioGuard")
-		gui.set_size_request(600,150)
+		gui.set_size_request(600,250)
 		gui.set_position(gtk.WIN_POS_CENTER)
 		gui.connect("delete-event", gtk.main_quit)
 	
@@ -23,6 +120,21 @@ class GUI_app():
 		vbox_main = gtk.VBox()
 		vbox1 = gtk.VBox()
 		vbox2 = gtk.VBox()
+		hbox_valores1 = gtk.HBox()
+		hbox_valores2 = gtk.HBox()
+		vbox_gpios = []
+		labels_gpios = []
+		for i in range(8):
+			labels_gpios.append([gtk.Label(),gtk.Label()])
+			vbox_gpios.append(gtk.VBox())
+			labels_gpios[i][0].set_text("GPIO"+pines[i])
+			vbox_gpios[i].pack_start(labels_gpios[i][0])
+			vbox_gpios[i].pack_start(labels_gpios[i][1])
+			if (i < 4):
+				hbox_valores1.pack_start(vbox_gpios[i])
+			else:
+				hbox_valores2.pack_start(vbox_gpios[i])
+		
 		hbox_botones = gtk.HBox()
 		hbox_salir = gtk.HBox()
 	
@@ -33,15 +145,21 @@ class GUI_app():
 		boton_salir = gtk.Button("SALIR")
 		boton_salir.connect('clicked',lambda a: gtk.main_quit())
 		label_nombre = gtk.Label("Nombre")
-		label_ciclo = gtk.Label("Ciclo")
+		label_ciclo = gtk.Label("Ciclo(min)")
 	
-		if (self.Estado() == 1): #Actualmente midiendo
+		if (Estado() == 1): #Actualmente midiendo
 			valor_estado = gtk.Label()
 			valor_estado.set_markup('<span color="green">CENSANDO</span>');
-			entry_nombre = gtk.Label(self.Nombre())
-			entry_ciclo = gtk.Label(self.Ciclo())
+			entry_nombre = gtk.Label(Nombre())
+			entry_ciclo = gtk.Label(Ciclo())
 			#Establezco la conexion entre botones y funciones
-			boton_detener.connect("clicked",lambda a:self.TerminarCensado())
+			boton_inicio.connect("clicked",lambda a:GUI_Error(errores[0]))
+			boton_detener.connect("clicked",lambda a:TerminarCensado(0))
+			boton_bajar.connect("clicked",lambda a:GUI_Error(errores[1]))
+			#for i in range(8):
+			#	labels_gpios[i][1] = subprocess.Popen(["sudo","/home/pi/Desktop/Python/Adafruit_DHT2","11",pines[i]],stdout=subprocess.PIPE).communicate()[0]
+
+	
 		else: #No esta midiendo
 			valor_estado = gtk.Label()
 			valor_estado.set_markup('<span color="red">SIN CENSAR</span>');
@@ -50,8 +168,11 @@ class GUI_app():
 			entry_nombre.set_text("Ingrese el nombre para ser guardaro..")
 			entry_ciclo.set_text("Tiempo en minutos entre medidas...")
 			#Establezco la conexion entre botones y funciones
-			boton_inicio.connect('clicked',lambda a:self.IniciarCensado(entry_nombre,entry_ciclo))
-			boton_bajar.connect('clicked',lambda a:self.BajarDatos())
+			boton_inicio.connect('clicked',lambda a:IniciarCensado(entry_nombre.get_text(),int(entry_ciclo.get_text(),0)))
+			boton_detener.connect("clicked",lambda a:GUI_Error(errores[2]))
+			boton_bajar.connect('clicked',lambda a:BajarDatos())
+			for i in range(8):
+				labels_gpios[i][1].set_text("-")
 					
 		#Empaqueto los botones,labels y entradas en los boxes
 		vbox1.pack_start(label_estado)
@@ -70,6 +191,8 @@ class GUI_app():
 		hbox_botones.pack_start(boton_bajar)
 	
 		vbox_main.pack_start(hbox_main)
+		vbox_main.pack_start(hbox_valores1)
+		vbox_main.pack_start(hbox_valores2)
 		vbox_main.pack_start(hbox_botones)
 		vbox_main.pack_start(boton_salir)
 	
@@ -78,101 +201,66 @@ class GUI_app():
 		gui.show_all()
 		gtk.main()
 		
+class Terminal():
+	"""
+	Permite ejecutar la aplicacion en version terminal
+	"""
+	comandos = ["ayuda","iniciar","terminar","bajar","status","salir"]
+	
+	def __init__(self):
+		comando = raw_input("BIOGUARD $ ").split(" ")
+		while (comando[0] != "salir"):
+			if (comando[0] == "iniciar"): #Inicio de sesion
+				if (len(comando) >= 3): #Cantidad de comandos correctos
+					if (Estado() == 0): #No se esta censando acutalmente
+						if (type(comando[2] == "int")): #El ciclo es un numero
+							IniciarCensado(comando[1],int(comando[2]),1)
+							print("Sesion iniciada...")
+						else:
+							print("ciclo incorrecto...")
+					else:
+						print(errores[0])
+				else:
+					print("Argumentos faltantes...")
 		
-	def IniciarCensado(self,widget1,widget2):
-		"""
-		Inicia el ciclo de sensado, los datos son almacenados cada "ciclo" minutos
-		bajo el nombre de "nombre", en caso de que se este actualmente censado se 
-		retorna -1.
-		"""
-		db = PostgreSQL.PostgreSQL()
-		nombre = widget1.get_text()
-		ciclo =  int(widget2.get_text())
-		try: #Intento modificar control a la db
-			row = db.SelectFromTable(ctr,["id",1])
-			if (row[0][4] == 0):
-				db.UpdateRegisterInTable(ctr,["id",1],["name",nombre])
-				db.UpdateRegisterInTable(ctr,["id",1],["ciclo",ciclo-1])
-				db.UpdateRegisterInTable(ctr,["id",1],["veces",0])
-				db.UpdateRegisterInTable(ctr,["id",1],["status",1])
+			elif(comando[0] == "terminar"): #Terminar sesion activa
+				if (Estado() == 1):
+					TerminarCensado(1)
+					print("Sesion terminada...")
+				else:
+					print(error[2])
+		
+			elif(comando[0] == "status"): #Imprimo los valores actuales de la sesion si esta activa
+				if (Estado() == 1):
+					print("	-Estado --> Corriendo")
+					print("	-Nombre --> %s" %(Nombre()))
+					print("	-Ciclo --> %i" %(Ciclo()))
+				else:
+					print("	-Estado --> Detenido")
+					
+			elif(comando[0] == "bajar"): #Bajo los datos de la sesion actual
+				if (Estado() == 0):
+					BajarDatos()
+					print("Datos guardados correctamente...")
+				else:
+					print(errores[1])
+			
+			elif(comando[0] == "ayuda"): #Imprimo ayuda en pantalla
+				print("	-ayuda --> Menu de ayuda")
+				print("	-iniciar nombre ciclo --> Crea una nueva sesion")
+				print("	-terminar --> Termina la sesion actual")
+				print("	-bajar --> Baja los datos de la sesion actual")
+				print("	-status --> Muestra el estado del sistema ")
+			
 			else:
-				return -1	
-		except: #No existe control
-			db.CreateTable(ctr,["name VARCHAR","ciclo INTEGER","veces INTEGER","status INTEGER"])
-			db.InsertRegisterInTable(ctr,[nombre,ciclo-1,0,1])
-		
-		db.CloseDB()
-		gtk.main_quit()
-		os.system('./Aplicacion.py')
-		return 0
-		
-	def TerminarCensado(self):
-		"""
-		Cancela el censado actual, en caso de no existir una etapa de censado
-		actual retorna -1
-		"""
-		db = PostgreSQL.PostgreSQL()
-		row = db.SelectFromTable(ctr,["id",1])
-		if (row[0][4] == 1):
-			db.UpdateRegisterInTable(ctr,["id",1],["status",0])
-			gtk.main_quit()
-			os.system('./Aplicacion.py')
-			db.CloseDB()
-			return 0
-		else:
-			return -1
-		db.CloseDB()
-		
+				print("Comando desconocido...")
 	
-	def BajarDatos(self): 
-		"""
-		Baja los datos de la db del ulitmo ciclo de censado, es decir donde el 
-		nombre es el guardado el almacenado en control.
-		En caso de que el ciclo de censado anterior no haya concluido retorna -1
-		"""
-		db = PostgreSQL.PostgreSQL()
-		row = db.SelectFromTable(ctr,["id",1])
-		if (row[0][4] == 0):
-			rows = db.SelectFromTable("register",["name",row[0][1]])
-			if (len(rows) > 0):
-				f = open(row[0][1] + "_datos.txt","w")
-				for elem in rows:
-					line = elem[1] + separador + str(elem[2]) + separador + str(elem[3]) + separador + str(elem[4]) + separador + str(elem[5])
-					f.write(line + "\n")
-				f.close()
-			db.CloseDB()
-			return 0
-		else:
-			return -1
-		
-	def Estado(self):
-		"""
-		Retorna el estado actual del censado
-		"""
-		db = PostgreSQL.PostgreSQL()
-		row = db.SelectFromTable(ctr,["id",1])
-		if (row[0][4] == 1):
-			db.CloseDB()
-			return 1
-		else:
-			return 0
-		
-	def Nombre(self):
-		"""
-		Retorna el nombre del censado actual
-		"""
-		db = PostgreSQL.PostgreSQL()
-		row = db.SelectFromTable(ctr,["id",1])
-		return row[0][1]
-	
-	def Ciclo(self):
-		"""
-		Retorna el ciclo del censado actual
-		"""
-		db = PostgreSQL.PostgreSQL()
-		row = db.SelectFromTable(ctr,["id",1])
-		db.CloseDB()
-		return row[0][2] + 1
-	
+			comando = raw_input("BIOGUARD $ ").split(" ")
+		sys.exit()
+
 if __name__ == "__main__":
-	GUI_app()
+	if (len(sys.argv) >= 2):
+		if (sys.argv[1] == "-t"):
+			Terminal()
+	else:
+		GUI_app()
