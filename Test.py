45297bb9b93c3cb94ca74c5de7614c527594b99e
Pablo <p.martinezben@gmail.com>
2013-08-31 11:53:59 -0300

    Added a new script to test if sensors are present

--- Test.py
+++ Test.py
@@ -12,15 +12,15 @@ pins = ["4","17","18","21","22","23","24","25"]
 values = []
 
 try: #Intento correr el script
-        try: #Encuentro el argumento del sensor
+        if (len(sys.argv) >= 2): #Encuentro el argumento del sensor
                 for i in range(len(pins)):
                         salida = subprocess.Popen(["sudo","./Adafruit_DHT2",sys.argv[1],pins[i]],stdout=subprocess.PIPE).communicate()[0]
                         values.append(salida)
                         if (values[i] != ""): #Hay sensor en ese pin
-                                print("PIN: %s --> STATUS: %s"%(pins[i],status[0]))
+                                print("GPIO_%s --> STATUS: %s"%(pins[i],status[0]))
                         else: #No hay sensor conectado
-                                print("PIN: %s --> STATUS: %s"%(pins[i],status[1]))
-        except: #Falta argumento del sensor
+                                print("GPIO_%s --> STATUS: %s"%(pins[i],status[1]))
+        else: #Falta argumento del sensor
                 print("Tipo de sensor faltante, ejecute: sudo ./Test.py sensor")
 except: #No se corrio con privilegios de root
         print("El test debe se corrido como root, ejecute: sudo ./Test.py sensor")
